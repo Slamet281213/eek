4da9c2e7524b3ac4b3aebb0019ace845bb6bc7cd
Slamet281213 <54432084+Slamet281213@users.noreply.github.com>
2021-03-17 17:25:33 +0700

    Add files via upload

--- example.php
+++ example.php
@@ -1,21 +1,16 @@
-# Cek-NIK-KTP
-Data yang dihasilkan bersumber dari data.kpu.go.id
-<h3>Usage</h3>
-<pre>&lt;?php
+<?php
 
 // include data-kpu.php
 require('data-kpu.php');
 
 // memasukan NIK 
 $nik = '1234xxxxx';
-    
+	
 // menjalankan proses curl dan return array
 $data = cek_ktp($nik);
 // print_r($data);
-    
+	
 // merubah data array menjadi json
 echo json_encode($data);
-    
-?&gt;</pre>
-
-<a href='http://ibacor.com/widget'><h3>DEMO</h3></a>
+	
+?>
